e1ac503993177e07951727099475d366117b205f
Nick Young <nick.young@auckland.ac.nz>
2020-04-08 10:40:47 +1200

    batch detection

--- detect_cars.py
+++ detect_cars.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 
 from imageai.Detection import ObjectDetection
+import os
+import json
+from tqdm.auto import tqdm
 
 detector = ObjectDetection()
-
-model_path = "./models/resnet50_coco_best_v2.0.1.h5"
-input_path = "./images/709/2020-04-03-080025.jpg"
-output_path = "./test_night.jpg"
-
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath(model_path)
+detector.setModelPath("./models/resnet50_coco_best_v2.0.1.h5")
 detector.loadModel()
+print("Detector ready")
 custom_objects = detector.CustomObjects(car=True, motorcycle=True, bus=True, truck=True)
-detection = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=input_path, output_image_path=output_path, minimum_percentage_probability=40)
 
-for eachItem in detection:
-    print(eachItem["name"] , " : ", eachItem["percentage_probability"])
+camIds = os.listdir("images")
+
+for camId in tqdm(camIds):
+    os.makedirs(f"annotations/{camId}", exist_ok = True)
+    images = os.listdir(f"images/{camId}/")
+    for image in tqdm(images):
+        if not image.endswith(".jpg"):
+            continue
+        image_path = f"images/{camId}/{image}"
+        output_path = f"annotations/{camId}/{image}.json"
+        if os.path.isfile(output_path): # skip done
+            continue
+        detected_image_array, detections = detector.detectCustomObjectsFromImage(output_type="array", custom_objects=custom_objects, input_image=image_path, minimum_percentage_probability=30)
+        with open(output_path, "w") as f:
+            json.dump(detections, f)
