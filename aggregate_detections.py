781b0654026c11d36d7fe446560ecdefeb414270
Nick Young <nick.young@auckland.ac.nz>
2020-04-20 04:51:24 +0000

    initial day level aggregate counts

--- aggregate_detections.py
+++ aggregate_detections.py
@@ -2,24 +2,28 @@
 
 import json
 import os
+from tqdm.auto import tqdm
 
 with open("cameras.json") as f:
     cameras = json.load(f)
 
-for i,c in enumerate(cameras["features"]):
+for i,c in enumerate(tqdm(cameras["features"])):
     camId = c["properties"]["id"]
     if not camId in os.listdir("annotations"):
         continue
     annotations = os.listdir(f"annotations/{camId}/")
     detections = {}
-    for annotation_path in annotations:
+    for annotation_path in tqdm(annotations):
         with open(f"annotations/{camId}/{annotation_path}") as f:
             a = json.load(f)
-        dt = annotation_path.split(".")[0]
-        vehicleSummary = {}
-        for vehicle in a:
-            vehicleSummary[vehicle["name"]] = vehicleSummary.get(vehicle["name"], 0) + 1
-        detections[dt] = vehicleSummary
+        dt = annotation_path[:10]
+        if dt not in detections:
+            detections[dt] = {
+                    "vehicles": 0,
+                    "n_images": 0
+            }
+        detections[dt]["vehicles"] += len(a)
+        detections[dt]["n_images"] += 1
     cameras["features"][i]["detections"] = detections
 
 with open("cameras_with_detections.json", "w") as f:
