001360ebce0759e2d6878a7b6dd5fc310ac3e4ce
Nick Young <nick.young@auckland.ac.nz>
2020-04-28 00:28:07 +0000

    sample annotations from mysql

--- sample_annotations.py
+++ sample_annotations.py
@@ -3,28 +3,61 @@
 import os
 import random
 import json
-from tqdm.auto import tqdm
+import mysql.connector
+import config
+import datetime
+
+db = mysql.connector.connect(
+  host="localhost",
+  user=config.user,
+  passwd=config.passwd,
+  database="car"
+)
+cur = db.cursor()
 
 random.seed(9001)
 
-camIds = sorted(os.listdir("annotations"))
+if os.path.isfile("dts_per_camID.json"):
+    with open("dts_per_camID.json") as f:
+        dts_per_camID = json.load(f)
+else:
+    cur.execute("SELECT camID, datetime FROM `detections`")
+    camIDs_and_datetimes = cur.fetchall()
+    dts_per_camID = {}
+    for camID, dt in camIDs_and_datetimes:
+        if camID not in dts_per_camID:
+            dts_per_camID[camID] = {}
+        date_str = str(dt.date())
+        if date_str not in dts_per_camID[camID]:
+            dts_per_camID[camID][date_str] = []
+        dts_per_camID[camID][date_str].append(str(dt))
+
+    with open("dts_per_camID.json", "w") as f:
+        json.dump(dts_per_camID, f)
+
+where = []
+for camID in dts_per_camID:
+    dts_for_camID = []
+    for day, dts in dts_per_camID[camID].items():
+        if len(dts) > 2:
+            dts = random.sample(dts, 2)
+        for dt in dts:
+            where.append(f"({camID},'{str(dt)}')")
+
+where = ",".join(where)
+sql = f"SELECT camID,datetime,detections FROM detections WHERE (camID,datetime) IN ({where})"
+cur.execute(sql)
+detections = cur.fetchall()
 
 results = {}
 
-for camId in tqdm(camIds):
-    annotations = os.listdir(f"annotations/{camId}")
-    unique_days = set()
-    results[camId] = {}
-    for a in annotations:
-        unique_days.add(a[:10])
-    for d in sorted(unique_days):
-        annotations_for_day = [a for a in annotations if a.startswith(d)]
-        if len(annotations_for_day) > 2:
-            annotations_for_day = random.sample(annotations_for_day, 2)
-        for annotation_path in annotations_for_day:
-            with open(f"annotations/{camId}/{annotation_path}") as f:
-                a = json.load(f)
-                for e in a:
-                    e["percentage_probability"] = round(e["percentage_probability"],2)
-            results[camId][annotation_path] = a
+for row in detections:
+    if row[0] not in results:
+        results[row[0]] = {}
+    data = json.loads(row[2])
+    for d in data:
+        d["percentage_probability"] = round(d["percentage_probability"], 2)
+    date_str = row[1].strftime("%Y-%m-%d-%H%M%S")
+    results[row[0]][date_str] = data
+
 print(json.dumps(results))
--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ git pull
 echo "starting aggregation"
 ./aggregate_detections.py
 echo "sampling"
+rm dts_per_camID.json
 ./sample_annotations.py > sample_annotations.json
 git commit -am "auto update"
 git push
