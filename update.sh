0d5216aa85e5a4eea49e751550974702c35577cf
Nick Young <nick.young@auckland.ac.nz>
2021-02-15 02:48:26 +0000

    set author for commits

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ echo "starting aggregation"
 echo "sampling"
 rm dts_per_camID.json
 ./sample_annotations.py > sample_annotations.json
-git commit -am "auto update"
+git commit -am "auto update" --author "car-detector-bot <ubuntu@cardetector.nzta-traffic-cameras.cloud.edu.au>"
 git push
